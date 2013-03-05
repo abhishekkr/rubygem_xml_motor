8e907701d788e7bb786df4268558f4686fb78602
abhishekkr <abhikumar163@gmail.com>
2013-03-05 14:30:56 +0530

    XMLIndexHandler expand_node_indexes refactored

--- lib/xml-motor/xml-index-handler.rb
+++ lib/xml-motor/xml-index-handler.rb
@@ -12,7 +12,7 @@ module XMLIndexHandler
   end
 
   def self.traverse_tag(xml_idx_to_find, tag)
-    tag.split(".")[1..-1].each do |tag_i|
+    tag.split('.')[1..-1].each do |tag_i|
       x_curr = XMLMotorEngine.xmltags[tag_i].values.flatten
       xml_idx_to_find = expand_node_indexes xml_idx_to_find, x_curr
     end
@@ -20,18 +20,25 @@ module XMLIndexHandler
   end
 
   def self.expand_node_indexes(outer_idx, x_curr)
-    osize = outer_idx.size/2 -1
-    xsize = x_curr.size/2 -1
     expanded_node_indexes = []
     (0...outer_idx.size).step(2) do |o|
       o1, o2 = outer_idx[o], outer_idx[o + 1]
-      (0...x_curr.size).step(2) do |x|
-        x1, x2 = x_curr[x], x_curr[x + 1]
-        next if o1 > x1 || o2 < x2
-        expanded_node_indexes.push x1
-        expanded_node_indexes.push x2
-      end
+      expanded_node_indexes += node_indexes_in_range(x_curr, o1, o2)
     end
     expanded_node_indexes.flatten
+  end
+
+  def self.node_indexes_in_range(x_curr, outer_open, outer_close)
+    node_indexes = []
+    (0...x_curr.size).step(2) do |x|
+      x1, x2 = x_curr[x], x_curr[x + 1]
+      next if node_index_out_of_bound?(outer_open, outer_close, x1, x2)
+      node_indexes.push x1, x2
+    end
+    node_indexes
+  end
+
+  def self.node_index_out_of_bound?(outer_min, outer_max, curr_min, curr_max)
+    outer_min > curr_min || outer_max < curr_max
   end
 end
