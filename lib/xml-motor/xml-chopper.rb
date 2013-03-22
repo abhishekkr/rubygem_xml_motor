bc2ff7dc528ce8ebe69e7beaaf9afc6a75fcc7a5
abhishekkr <abhikumar163@gmail.com>
2013-03-22 16:29:16 +0530

    XMLChopper.get_attribute_hash refactored

--- lib/xml-motor/xml-chopper.rb
+++ lib/xml-motor/xml-chopper.rb
@@ -16,19 +16,24 @@ module XMLChopper
     attrs = {}
   begin
     broken_attrib = attribzone.split(/=/)
-    attribs = broken_attrib.first.strip
-    values = nil
-    broken_attrib[1..-2].each do |attrib_part|
-      value_n_attrib = attrib_part.split
-      values = value_n_attrib[0..-2].join(' ')
-      attrs[attribs] = XMLUtils.dbqot_string values
-      attribs = value_n_attrib[-1].strip
-    end
-    values = broken_attrib.last.strip
-    attrs[attribs] = XMLUtils.dbqot_string values
+    attrs = map_value_attrib(broken_attrib.first.strip,
+                             broken_attrib[1..-2], broken_attrib.last.strip)
   rescue
     return attrs
   end
     attrs
+  end
+
+  def self.map_value_attrib(first_key, mid_attrib, last_val)
+    attrib_map, attribs, values = {}, [first_key], []
+    mid_attrib.each do |prev_value_n_next_key|
+      prev_value_n_next_key = prev_value_n_next_key.split
+      next unless prev_value_n_next_key.class == Array
+      values  << XMLUtils.dbqot_string( prev_value_n_next_key[0..-2].join(' ') )
+      attribs << prev_value_n_next_key[-1].strip
+    end
+    values << XMLUtils.dbqot_string( last_val )
+    attribs.zip(values){|key, val| attrib_map[key] = val }
+    attrib_map
   end
 end
